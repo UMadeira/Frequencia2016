f8bfdd766be9afe8a3e96309ff6987de892f45fd
Leonel Nóbrega <lnobrega@uma.pt>
2017-01-12 00:16:49 +0000

    Question 8

---
+++ Library/Model/IObservable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public delegate void NotifyHandler( IObservable aObservable, object aData );
+
+    public interface IObservable
+    {
+        event NotifyHandler OnNotify;
+
+        void Notify( object aData = null );
+    }
+}
---
+++ Library/Model/Observables/ObservableBook.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model.Observables
+{
+    class ObservableBook : IBook, IObservable
+    {
+        public ObservableBook( IBook aBook )
+        {
+            Book = aBook;
+        }
+
+        private IBook Book { get; }
+
+        public string ISBN => Book.ISBN;
+
+        public string Title
+        {
+            get { return Book.Title; }
+            set { Book.Title = value; Notify(); }
+        }
+
+        public string Summary
+        {
+            get { return Book.Summary; }
+            set { Book.Summary = value; Notify(); }
+        }
+
+        public IEnumerable<IAuthor> Authors => Book.Authors;
+
+        public void Append( IAuthor aAuthor )
+        {
+            Book.Append( aAuthor );
+            Notify();
+        }
+        public void Remove( IAuthor aAuthor )
+        {
+            Book.Remove( aAuthor );
+            Notify();
+        }
+        public IEnumerable<IBookItem> Items => Book.Items;
+
+        public void Append( IBookItem aItem )
+        {
+            Book.Append( aItem );
+            Notify();
+        }
+
+        public void Remove( IBookItem aItem )
+        {
+            Book.Remove( aItem );
+            Notify();
+        }
+
+        public event NotifyHandler OnNotify;
+
+        public void Notify( object aData = null )
+        {
+            OnNotify?.Invoke( this, aData );
+        }
+    }
+}
